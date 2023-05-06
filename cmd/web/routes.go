50c3e15779d733493117d97e5349fc2bb8631109
Henry Tanoh <thenry702@gmail.com>
2023-05-06 22:38:04 +0200

    Update of routes and play with post and get handlers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bookings/pkg/config"
-	"bookings/pkg/handlers"
+	"bookings/internal/config"
+	"bookings/internal/handlers"
 	"net/http"
 
 	"github.com/go-chi/chi"
@@ -18,8 +18,14 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	mux.Get("/search-availability-json", handlers.Repo.AvailabilityJSON)
+	mux.Get("/contact", handlers.Repo.Contact)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
-	mux.Handle("./static/*", http.StripPrefix("/static", fileServer))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
